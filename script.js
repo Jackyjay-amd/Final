d2e9f075b461e271d6bd0dd9566a69eff371f6c8
Jackyjay-amd <jiaxuzhai807@gmail.com>
2025-01-10 15:36:35 +1030

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function changeImage(imageNumber) {
 
   switch (imageNumber) {
     case 1:
-      image.src = "messi1.jpg";
+      image.src = "https://assets.goal.com/images/v3/blt6ad8e1581c7cefd6/Lionel_Messi_Argentina_2022.jpg";
       description.textContent = "Lionel Messi scoring a stunning free kick.";
       break;
     case 2:
-      image.src = "messi2.jpg";
+      image.src = "https://assets.goal.com/images/v3/blt6ad8e1581c7cefd6/Lionel_Messi_Argentina_2022.jpg";
       description.textContent = "Messi celebrating a goal with Barcelona.";
       break;
     case 3:
-      image.src = "messi3.jpg";
+      image.src = "https://assets.goal.com/images/v3/blt6ad8e1581c7cefd6/Lionel_Messi_Argentina_2022.jpg";
       description.textContent = "Messi lifting the World Cup trophy.";
       break;
     default:
